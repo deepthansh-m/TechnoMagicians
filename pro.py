14d5a43b8a1f8ebfeaa15f0ce5ef5106aaf3d5bb
deepthansh-m <deepthanshm@gmail.com>
2024-01-25 02:23:28 +0530

    final

--- pro.py
+++ pro.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from textblob import TextBlob
 import instaloader
+from PIL import Image
+from io import BytesIO
+import requests 
 
 def analyze_sentiment(text):
     analysis = TextBlob(text)
@@ -16,23 +19,24 @@ def profile_user(username):
         total_posts = profile.mediacount
         followers = profile.followers
         following = profile.followees
+        profile_pic_url = profile.profile_pic_url
         
         if not user_posts:
-            return "No Posts with Caption", 0, total_posts, followers, following
+            return "No Posts with Caption", 0, total_posts, followers, following, profile_pic_url
         
         sentiments = [analyze_sentiment(post) for post in user_posts]
         average_sentiment = sum(sentiments) / len(sentiments)
         risk = average_sentiment * 100
 
         if risk >= 20:
-            return "High Risk", risk, total_posts, followers, following
+            return "High Risk", risk, total_posts, followers, following, profile_pic_url
         elif -20 < risk < 20:
-            return "Medium Risk", risk, total_posts, followers, following
+            return "Medium Risk", risk, total_posts, followers, following, profile_pic_url
         else:
-            return "Low Risk", risk, total_posts, followers, following
+            return "Low Risk", risk, total_posts, followers, following, profile_pic_url
 
     except instaloader.exceptions.ProfileNotExistsException as e:
-        return f"Error: {e}", 0, 0, 0, 0
+        return f"Error: {e}", 0, 0, 0, 0, ""
 
 def main():
     st.title("Instagram Profile Analyzer")
@@ -42,14 +46,15 @@ def main():
 
     if st.button("Analyze"):
         # Profile the user and display the results
-        risk_level, risk_percentage, total_posts, followers, following = profile_user(username_to_analyze)
-
+        risk_level, risk_percentage, total_posts, followers, following, profile_pic_url = profile_user(username_to_analyze)
+        if profile_pic_url:
+            image = Image.open(BytesIO(requests.get(profile_pic_url).content))
+            st.image(image, caption='Profile Picture', use_column_width=True)
         st.write(f"User: {username_to_analyze}")
         st.write(f"Risk Level: {risk_level}")
         st.write(f"Risk Percentage: {risk_percentage:.2f}%")
         st.write(f"Total Posts: {total_posts}")
         st.write(f"Followers: {followers}")
         st.write(f"Following: {following}")
-
 if __name__ == "__main__":
     main()
